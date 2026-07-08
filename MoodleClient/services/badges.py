5b798bcbf6025a3ddea3bdb638d6ac41083fb3fa
Leonardo Miranda Gomes <l.mirandagomes.inf25@stud.bbbaden.ch>
2026-07-08 22:28:23 +0200

    implemented the parameters for get_user_badges

--- MoodleClient/services/badges.py
+++ MoodleClient/services/badges.py
@@ -1,13 +1,25 @@
 import logging
 
 from ..models.badges import BadgeStructure
-from .base import BaseService
+from .base import BaseService, auto_moodle_params
 
 logger = logging.getLogger(__name__)
 
 
 class BadgesService(BaseService):
-    async def get_user_badges(self) -> BadgeStructure:
+    @auto_moodle_params
+    async def get_user_badges(
+        self,
+        user_id: int | None = None,
+        course_id: int | None = None,
+        page: int | None = None,
+        per_page: int | None = None,
+        search: str | None = None,
+        only_public: bool | None = None,
+        data: dict | None = None,
+    ) -> BadgeStructure:
         logger.info("Fetching user badges...")
-        response = await self.session.request("core_badges_get_user_badges")
+        response = await self.session.request(
+            "core_badges_get_user_badges", extra_params=data
+        )
         return self._parse_response(response, BadgeStructure)
