a0381242c310d498efa423f4a735acdbd5633b52
Leonardo Miranda Gomes <l.mirandagomes.inf25@stud.bbbaden.ch>
2026-07-09 15:48:14 +0200

    import the needed classes onto the MoodleClient namespace

---
+++ MoodleClient/__init__.py
@@ -0,0 +1,14 @@
+from .auth import MoodleCredentials, MoodleTokenAuth, MoodleTokens
+from .client import MoodleClient
+from .config import AppConfig
+from .session import MoodleSession
+
+__all__ = [
+    MoodleCredentials,
+    MoodleTokens,
+    MoodleTokenAuth,
+    MoodleClient,
+    MoodleSession,
+    AppConfig,
+    MoodleSession,
+]
