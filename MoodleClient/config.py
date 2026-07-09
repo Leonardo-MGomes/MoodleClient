b152e25b451848c422350e70bddc3f7946097b09
Leonardo Miranda Gomes <l.mirandagomes.inf25@stud.bbbaden.ch>
2026-07-09 15:47:48 +0200

    set the default value to a sandbox/example moodle URL

--- MoodleClient/config.py
+++ MoodleClient/config.py
@@ -1,15 +1,18 @@
 import os
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True)
 class AppConfig:
     BASE_URL: str
     USER_AGENT: str
 
+
 def create_config() -> AppConfig:
-    base_url = os.getenv("MOODLE_BASE_URL", "https://moodle.bbbaden.ch")
+    base_url = os.getenv("MOODLE_BASE_URL", "https://school.moodledemo.net")
     user_agent = os.getenv("MOODLE_USER_AGENT", "moodle-downloader/0.1a4")
 
     return AppConfig(BASE_URL=base_url, USER_AGENT=user_agent)
+
 
 DEFAULT_CONFIG = create_config()
