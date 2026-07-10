2bb336c6182ec6579bd14af5aa89a88a0c660663
Leonardo Miranda Gomes <l.mirandagomes.inf25@stud.bbbaden.ch>
2026-07-10 13:12:47 +0200

    Implemented BadgeClass fix: GetBadge actually uses BadgeClass and NOT UserBadge.

--- MoodleClient/models/badges.py
+++ MoodleClient/models/badges.py
@@ -1,9 +1,31 @@
 from datetime import timedelta
+from typing import Literal
 
 from pydantic import BaseModel, EmailStr, HttpUrl
 
 from .common import HashStr, MoodleDateTime, MoodleWarnings
 from .enums import BadgeStatus, BadgeType
+
+
+# https://github.com/moodle/moodle/blob/MOODLE_502_STABLE/public/badges/classes/external/badgeclass_exporter.php#L39
+# MOODLE_502_STABLE badgeclass_exporter
+class BadgeClass(BaseModel):
+    type: Literal["BadgeClass"] = "BadgeClass"
+    id: HttpUrl  # No, this isn't wrong: https://github.com/moodle/moodle/blob/main/public/lib/badgeslib.php#L540
+    issuer: str | None = None
+    name: str
+    image: HttpUrl
+    description: str
+    hostedUrl: HttpUrl | None = None
+    courseid: int | None = None
+    coursefullname: str | None = None
+
+
+# https://github.com/moodle/moodle/blob/MOODLE_502_STABLE/public/badges/classes/external/get_badge.php#L99
+# MOODLE_502_STABLE execute_returns
+class GetBadgeStructure(BaseModel):
+    badge: BadgeClass
+    warnings: list[MoodleWarnings]
 
 
 # https://github.com/moodle/moodle/blob/MOODLE_502_STABLE/public/badges/classes/external/user_badge_exporter.php#L43
@@ -41,13 +63,6 @@ class UserBadge(BaseModel):
     version: str | None = None
     language: str | None = None
     imagecaption: str | None = None
-
-
-# https://github.com/moodle/moodle/blob/MOODLE_502_STABLE/public/badges/classes/external/get_badge.php#L99
-# MOODLE_502_STABLE execute_returns
-class GetBadgeStructure(BaseModel):
-    badge: UserBadge
-    warnings: list[MoodleWarnings]
 
 
 # https://github.com/moodle/moodle/blob/MOODLE_502_STABLE/public/badges/classes/external.php#L143
